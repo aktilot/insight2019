b2bb7cca321f4f195e679ed8a04faa975750b503
aktilot <amanda.k.tilot@gmail.com>
2019-06-10 14:45:33 -0700

    Update prototyping_model.py
    Added ability to show text sources probability scores as a density plot. Can see that I need more "in the middle" data.

--- prototyping_model.py
+++ prototyping_model.py
@@ -10,9 +10,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import metrics
 import ijson
 import seaborn as sns
+import matplotlib.pyplot as plt
 #%%
 """
-Here we're loading in the datasets and munging
+Here we're loading in the datasets and labeling them.
 """
 #%%
 # hp #32,562 comments
@@ -39,19 +40,35 @@ enron["tone"] = "professional"
 enron["subreddit"] = "enron"
 #%%
 """
-Split the training (80%) and test (20%) data Building our model pipeline
+Clean the data.
 """
 #%%
 all_data = pd.concat([ah.loc[1:8000,["text","subreddit","tone", "score"]], 
                       hp.loc[1:8000,["text","subreddit","tone", "score"]],
                       enron.loc[:,["text","subreddit","tone", "score"]]],
-                    sort = False)
+                    sort=False, 
+                    ignore_index=True)
 all_data["text"] = all_data["text"].fillna(' ') #so that the TfidfVectorizer will work
 
-list_corpus = all_data["text"].tolist()
-list_labels = all_data["score"].tolist()
+# function to remove URLs from the data. May add more bits.
+# adapted from https://github.com/hundredblocks/concrete_NLP_tutorial/blob/master/NLP_notebook.ipynb
+def standardize_text(df, text_field):
+    df[text_field] = df[text_field].str.replace(r"http\S+", "")
+    df[text_field] = df[text_field].str.replace(r"http", "")
+    return df
 
-X_train, X_test, y_train, y_test = train_test_split(list_corpus, list_labels, 
+all_data["text"] = standardize_text(all_data, "text")
+
+
+#%%
+"""
+Split the training (80%) and test (20%) data Building our model pipeline
+"""
+#%%
+#list_corpus = all_data["text"].tolist()
+#list_labels = all_data["score"].tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(all_data["text"], all_data["score"], 
                                                     test_size=0.2, 
                                                     random_state=40)
 #%%
@@ -82,11 +99,37 @@ from sklearn.metrics import confusion_matrix
 confusion_matrix = confusion_matrix(y_test, y_pred)
 print(confusion_matrix)
 #%%
-
+"""
+Calculating probabilities and attaching them to the labels for the test set.
+"""
 #%%
-train_prob = pipeline_tfidf.predict_proba(X_test)[:,1]
-train_prob_labeled = pd.concat([train_prob, ])
+test_prob = pipeline_tfidf.predict_proba(X_test)[:,1]
+test_prob = pd.DataFrame(test_prob, columns = ["Probability"])
+y_labels = pd.DataFrame(y_test)
+y_labels = y_labels.join(all_data["subreddit"], how='left')
+y_labels = y_labels.reset_index()
+test_prob_labeled = y_labels.join(test_prob, how="left") 
+#%%
+"""
+Density plot with labels for each data source, to see how I'm doing.
+Code adapted from: https://towardsdatascience.com/histograms-and-density-plots-in-python-f6bda88f5ac0
+"""
+#%%
+subreddits = pd.unique(test_prob_labeled["subreddit"])
 
-sns.distplot(train_prob, hist = False, kde = True,
-                 kde_kws = {'linewidth': 3})
+# Iterate through the five airlines
+for subr in subreddits:
+    # Subset to the airline
+    subset = test_prob_labeled[test_prob_labeled['subreddit'] == subr]
+    
+    # Draw the density plot
+    sns.distplot(subset['Probability'], hist = False, kde = True,
+                 kde_kws = {'linewidth': 3},
+                 label = subr)
+    
+# Plot formatting
+plt.legend(prop={'size': 16}, title = 'Data source')
+plt.title('Density Plot with Multiple Text Sources')
+plt.xlabel('Probability of being labeled "Professional"')
+plt.ylabel('Density')
 #%%
