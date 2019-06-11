143d56a3136ef045e243d48bdc88e02bd8929232
aktilot <amanda.k.tilot@gmail.com>
2019-06-11 13:48:25 -0700

    Update prototyping_model.py
    Added functions for defining custom stopword sets and importing stopwords from nltk

--- prototyping_model.py
+++ prototyping_model.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #%%
-import sys
 import os
 import pandas as pd
 import numpy as np
-import json
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.linear_model import LogisticRegression
@@ -35,7 +33,7 @@ ah["score"] = 1
 ah["tone"] = "professional"
 
 # first 8,000 rows of the enron dataset
-enron = pd.read_csv("../data/enron_05_17_2015_with_labels_v2.csv", nrows = 8000) #517,401 emails, some are labeled with
+enron = pd.read_csv("../data/enron_05_17_2015_with_labels_v2.csv", nrows = 20000) #517,401 emails, some are labeled with
 enron.rename(columns={'content': 'text'}, inplace=True)
 enron["score"] = 1
 enron["tone"] = "professional"
@@ -44,6 +42,7 @@ enron["subreddit"] = "enron"
 """
 Set functions to create dataframes for subreddits of interest.
 This is very inefficient right now, SQL would be a better solution.
+Inspired by: https://stackoverflow.com/questions/30539679/python-read-several-json-files-from-a-folder
 """
 #%%
 # bash command to split giant reddit file
@@ -54,7 +53,9 @@ def multi_grep(subreddit, path_to_json):
     json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
     os.mkdir(path_to_json+subreddit)
     for i in json_files:
-        os.system("grep "+subreddit+" ../data/reddit_chunks/"+i+" > ../data/reddit_chunks/"+subreddit+"/"+subreddit+"_"+i)
+        out_file = "../data/reddit_chunks/"+subreddit+"/"+subreddit+"_"+i
+        if not os.path.isfile(out_file):
+            os.system("grep "+subreddit+" ../data/reddit_chunks/"+i+" > ../data/reddit_chunks/"+subreddit+"/"+subreddit+"_"+i)
         print(i)
     print("Done. Proceed to jsons_to_df")
     
@@ -77,19 +78,40 @@ Run Reddit functions on all subreddits I think might be helpful.
 #%%
 #multi_grep("fortnite","../data/reddit_chunks/")
 #fortnite = jsons_to_df("FORTnITE", "../data/reddit_chunks/FORTnITE/")
+subs_to_include = ["datascience","legaladvice", "pics"]
+#for i in subs_to_include:
+#    multi_grep(i, "../data/reddit_chunks/")
 
+datasci = jsons_to_df("datascience", "../data/reddit_chunks/datascience/")
+legadv = jsons_to_df("legaladvice", "../data/reddit_chunks/legaladvice/")
+pics = jsons_to_df("pics", "../data/reddit_chunks/pics/")
+
+datasci["score"] = 1
+datasci["tone"] = "professional"
+
+legadv["score"] = 1
+legadv["tone"] = "professional"
+
+pics["score"] = 0
+pics["tone"] = "unprofessional"
+pics = pics.iloc[0:20000,]
 
 #%%
 """
 Clean the data.
 """
 #%%
-all_data = pd.concat([ah.loc[1:8000,["text","subreddit","tone", "score"]], 
-                      hp.loc[1:8000,["text","subreddit","tone", "score"]],
-                      enron.loc[:,["text","subreddit","tone", "score"]]],
-                    sort=False, 
-                    ignore_index=True)
+sources = [hp, ah, datasci, legadv, pics, enron]
+subs_to_include = subs_to_include + ["enron", "harrypotter", "askhistorians"]
+#all_data = pd.concat([ah.loc[1:8000,["text","subreddit","tone", "score"]], 
+#                      hp.loc[1:8000,["text","subreddit","tone", "score"]],
+#                      enron.loc[:,["text","subreddit","tone", "score"]]],
+#                    sort=False, 
+#                    ignore_index=True)
+all_data = pd.concat(sources, sort = False, ignore_index=True)
+all_data = all_data[all_data["subreddit"].isin(subs_to_include)] #cleaning a few weird rows from enron
 all_data["text"] = all_data["text"].fillna(' ') #so that the TfidfVectorizer will work
+
 
 # function to remove URLs from the data. May add more bits.
 # adapted from https://github.com/hundredblocks/concrete_NLP_tutorial/blob/master/NLP_notebook.ipynb
@@ -99,6 +121,9 @@ def standardize_text(df, text_field):
     return df
 
 all_data["text"] = standardize_text(all_data, "text")
+
+all_data["subreddit"].value_counts()
+all_data["tone"].value_counts()
 
 
 #%%
@@ -174,3 +199,76 @@ plt.title('Density Plot with Multiple Text Sources')
 plt.xlabel('Probability of being labeled "Professional"')
 plt.ylabel('Density')
 #%%
+"""
+Testing TF-IDF importance - what's driving the classification? Do we need custom stopwords?
+Code adapted from the Insight blog.
+"""
+#%%
+def tfidf(data):
+    tfidf_vectorizer = TfidfVectorizer(ngram_range = (1, 2), lowercase = False, stop_words = 'english')
+    train = tfidf_vectorizer.fit_transform(data)
+    return train, tfidf_vectorizer
+
+def get_most_important_features(vectorizer, model, n=5):
+    index_to_word = {v:k for k,v in vectorizer.vocabulary_.items()}
+    
+    # loop for each class
+    classes ={}
+    for class_index in range(model.coef_.shape[0]):
+        word_importances = [(el, index_to_word[i]) for i,el in enumerate(model.coef_[class_index])]
+        sorted_coeff = sorted(word_importances, key = lambda x : x[0], reverse=True)
+        tops = sorted(sorted_coeff[:n], key = lambda x : x[0])
+        bottom = sorted_coeff[-n:]
+        classes[class_index] = {
+            'tops':tops,
+            'bottom':bottom
+        }
+    return classes
+
+X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
+
+clf_tfidf = LogisticRegression(C=30.0, class_weight='balanced', solver='newton-cg', 
+                         multi_class='multinomial', n_jobs=-1, random_state=40)
+clf_tfidf.fit(X_train_tfidf, y_train)
+
+importance_tfidf = get_most_important_features(tfidf_vectorizer, clf_tfidf, 20)
+#%%
+"""
+Custom stopword sets!
+"""
+#%%
+#def tfidf_by_sub(vectorizer, text):
+    
+def get_top_n_words(corpus, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text corpus.
+    Adapted from https://medium.com/@cristhianboujon/how-to-list-the-most-common-words-from-text-corpus-using-scikit-learn-dad4d0cab41d
+    """
+    vec = TfidfVectorizer(ngram_range = (1, 2), lowercase = False, stop_words = 'english').fit(corpus)
+    tfidf_vec = vec.transform(corpus)
+    sum_words = tfidf_vec.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in     vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+    
+custom_stopwords = {}
+
+for i in subs_to_include:
+    test_input = all_data.loc[all_data["subreddit"]==i,["text"]]
+    test_input = test_input.values.tolist()
+    test_input = [item for sublist in test_input for item in sublist]
+    print("working on "+i)
+    common_words = get_top_n_words(test_input, 20)
+    custom_stopwords[i] = common_words
+    
+from nltk.corpus import stopwords
+
+my_stopwords = []
+for i in subs_to_include:
+    stops = [lis[0] for lis in custom_stopwords[i]]
+    my_stopwords = my_stopwords + stops
+    
+my_stopwords = my_stopwords + stopwords.words('english')
+#%%    
+    
+    
